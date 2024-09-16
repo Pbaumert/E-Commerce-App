4578d9ab6af207c460206e991e6291672127dc9f
Paul Baumert <baumert.trey@gmail.com>
2024-09-15 21:14:12 -0500

    added logic for sequelize

--- models/index.js
+++ models/index.js
@@ -1,20 +1,12 @@
-// import models
-const Product = require('./Product');
 const Category = require('./Category');
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Define relationships
+Product.belongsTo(Category, { foreignKey: 'category_id' });
+Category.hasMany(Product, { foreignKey: 'category_id' });
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = { Category, Product, Tag, ProductTag };
